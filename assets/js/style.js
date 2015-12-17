5dfcbd42d1404dfc034def8561537f886e398a4e
Justin Mariner <justin.mariner@hotmail.com>
2015-12-17 13:41:17 -0500

    nav for phones

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,3 +1,9 @@
 $(function() {
 	$("nav a." + /^ren\-(.+)$/.exec($("body").attr("data-location"))[1]).addClass("current");
+	$("#openNav").click(function() {
+		$("header").addClass("navActive");
+	});
+	$("#closeNav").click(function() {
+		$("header").removeClass("navActive");
+	});
 });
