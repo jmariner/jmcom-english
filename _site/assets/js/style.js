6ade207da31b6cf46dd4edb04d3f1a68355137f1
Justin <justin.mariner@hotmail.com>
2015-12-18 05:50:58 -0500

    no longer sending _site

--- _site/assets/js/style.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$("nav a." + /^ren\-(.+)$/.exec($("body").attr("data-location"))[1]).addClass("current");
-	$("#openNav").click(function() {
-		$("header").addClass("navActive");
-	});
-	$("#closeNav").click(function() {
-		$("header").removeClass("navActive");
-	});
-});
