452a2b4e79a4c15ff1b3205c4877a7f8aa7070f8
Justin <justin.mariner@hotmail.com>
2016-04-04 06:19:14 -0400

    started custom docviewer

--- assets/docViewer/custom.js
+++ assets/docViewer/custom.js
@@ -4,23 +4,43 @@ $(function() {
 	$("body").append(pageCont).attr("id","page-container");
 
 	var lastPage = +$(".pf:last-child").attr("data-page-no");
+	$(".pf").attr("data-last-page", lastPage);
 	var pageHeight = $(".h0").height();
-	$("#page-container").height((pageHeight+50)*lastPage);
+	$("body").height((pageHeight+25)*lastPage);
 
 	var widthScale = 1;
 	var scale = (window.innerWidth * widthScale) / $(".w0").width();
 	$("body").css("transform", "scale("+scale+")");
 
-	document.body.curPage = .95;
+	document.body.curPage = 1;
 	document.body.lastPage = lastPage;
 
+	var $pageNav = $("<div>").addClass("page-nav")
+	.append($("<div>").addClass("prev").text("Prev").click(prevPage))
+	.append($("<div>").addClass("next").text("Next").click(nextPage));
+
+	$(".pf").append($pageNav.clone(true).addClass("top")).append($pageNav.clone(true).addClass("bottom"));
+
 	$(document).keydown(function(e) {
-		if (e.which === 37 || e.which === 39) {
-			if (e.which === 39 && document.body.curPage < document.body.lastPage) // right - next page
-				document.body.curPage++;
-			if (e.which === 37 && document.body.curPage > 1) // left - prev page
-				document.body.curPage--;
-			document.body.scrollTop = $("body").height() * (document.body.curPage-1);
+		if (e.which === 38 || e.which === 40) {
+			if (e.which === 40 && document.body.curPage < document.body.lastPage) // right - next page
+				nextPage();
+			if (e.which === 38 && document.body.curPage > 1) // left - prev page
+				prevPage();
 		}
-	})
+	});
 });
+
+function prevPage() {
+	if (document.body.curPage > 1) scrollToPage(--document.body.curPage);
+}
+
+function nextPage() {
+	if (document.body.curPage < document.body.lastPage) scrollToPage(++document.body.curPage);
+}
+
+function scrollToPage(p) {
+	var $p = $(".pf[data-page-no="+p+"]");
+	if ($p.length === 1)
+		document.body.scrollTop = $p.offset().top
+}
